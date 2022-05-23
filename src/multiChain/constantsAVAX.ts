d7923942f8090bda9d08e6c0355ca22492c84475
merlincdj <377730858@qq.com>
2022-05-23 16:06:46 +0800

    Add Avalanche stable tokens
    Fix

--- src/multiChain/constantsAVAX.ts
+++ src/multiChain/constantsAVAX.ts
@@ -19,10 +19,12 @@ export const WRAPPED = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7';
 
 export const STABLE_TOKENS: string[] = [
     "0x19860ccb0a68fd4213ab9d8266f7bbf05a8dde98",  // BUSD
-    "0xc7198437980c041c805a1edcba50c1ce5db95118",  // USDT
-    "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664",  // USDC
-    "0xd586e7f844cea2f87f50152665bcbc2c279d8d70",  // DAI
+    "0xc7198437980c041c805a1edcba50c1ce5db95118",  // USDT.e
+    "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664",  // USDC.e
+    "0xd586e7f844cea2f87f50152665bcbc2c279d8d70",  // DAI.e
     "0x1c20e891bab6b1727d14da358fae2984ed9b59eb",  // TUSD
+    "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7",  // USDT
+    "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",  // USDC
 ];
 
 // Contract Name as Variable Name

7aa14c416e058668383cb32c4e87063d15032888
zhangchitc <95238503+zhangchitc@users.noreply.github.com>
2025-02-11 15:05:50 +0800

    add comma

--- src/mappings/wooTokenOFT/index.ts
+++ src/mappings/wooTokenOFT/index.ts
@@ -11,32 +11,32 @@ import { Bytes } from "@graphprotocol/graph-ts"
 export function handleOFTReceived(event: OFTReceivedEvent): void {
   let entity = new OFTReceived(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.guid = event.params.guid
-  entity.srcEid = event.params.srcEid
-  entity.toAddress = event.params.toAddress
-  entity.amountReceivedLD = event.params.amountReceivedLD
+  );
+  entity.guid = event.params.guid;
+  entity.srcEid = event.params.srcEid;
+  entity.toAddress = event.params.toAddress;
+  entity.amountReceivedLD = event.params.amountReceivedLD;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOFTSent(event: OFTSentEvent): void {
   let entity = new OFTSent(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.guid = event.params.guid
-  entity.dstEid = event.params.dstEid
-  entity.fromAddress = event.params.fromAddress
-  entity.amountSentLD = event.params.amountSentLD
-  entity.amountReceivedLD = event.params.amountReceivedLD
+  );
+  entity.guid = event.params.guid;
+  entity.dstEid = event.params.dstEid;
+  entity.fromAddress = event.params.fromAddress;
+  entity.amountSentLD = event.params.amountSentLD;
+  entity.amountReceivedLD = event.params.amountReceivedLD;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }

d4d944ee1961c7ad69cce9fbdf14909d08909e5b
zhangchitc <95238503+zhangchitc@users.noreply.github.com>
2025-02-19 10:21:06 +0800

    [layerzero OFT] change graphQL schema

--- src/mappings/wooTokenOFT/index.ts
+++ src/mappings/wooTokenOFT/index.ts
@@ -12,14 +12,14 @@ export function handleOFTReceived(event: OFTReceivedEvent): void {
   let entity = new OFTReceived(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   );
-  entity.guid = event.params.guid;
+  entity.guid = event.params.guid.toHexString();
   entity.srcEid = event.params.srcEid;
-  entity.toAddress = event.params.toAddress;
+  entity.toAddress = event.params.toAddress.toHexString();
   entity.amountReceivedLD = event.params.amountReceivedLD;
 
   entity.blockNumber = event.block.number;
   entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.transactionHash = event.transaction.hash.toHexString();
 
   entity.save();
 }
@@ -28,15 +28,15 @@ export function handleOFTSent(event: OFTSentEvent): void {
   let entity = new OFTSent(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   );
-  entity.guid = event.params.guid;
+  entity.guid = event.params.guid.toHexString();
   entity.dstEid = event.params.dstEid;
-  entity.fromAddress = event.params.fromAddress;
+  entity.fromAddress = event.params.fromAddress.toHexString();
   entity.amountSentLD = event.params.amountSentLD;
   entity.amountReceivedLD = event.params.amountReceivedLD;
 
   entity.blockNumber = event.block.number;
   entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.transactionHash = event.transaction.hash.toHexString();
 
   entity.save();
 }
